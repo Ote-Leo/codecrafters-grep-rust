27f17e946cfef265005757482d23bc03468947c2
Ote Leo <ote.leo73@gmail.com>
2024-05-16 09:31:12 +0300

    Passing first stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::io;
-use std::process;
+use std::io::stdin;
+use std::process::exit;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
@@ -10,25 +10,20 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     }
 }
 
-// Usage: echo <input_text> | your_grep.sh -E <pattern>
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
+fn main() -> anyhow::Result<()> {
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
-        process::exit(1);
+        exit(1);
     }
 
     let pattern = env::args().nth(2).unwrap();
     let mut input_line = String::new();
 
-    io::stdin().read_line(&mut input_line).unwrap();
+    stdin().read_line(&mut input_line)?;
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if !match_pattern(&input_line, &pattern) {
+        exit(1)
+    }
+
+    Ok(())
 }
